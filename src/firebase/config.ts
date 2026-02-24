e8161824957f030afe5cbcf3264762366d91bf7a
Dharmik Anand Devarakonda <dharmikanand.d@gmail.com>
2026-02-24 17:08:26 +0000

    Fixed Firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,12 @@
 import { initializeApp, getApps } from "firebase/app";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyXXXX",
+  apiKey: "AIzaSyAQ0Jtbx1eedNAz71nASbSbtOuG6PsquqA",
   authDomain: "studio-2645965705-c0aaa.firebaseapp.com",
   projectId: "studio-2645965705-c0aaa",
-  appId: "1:543137308160:web:XXXX",
-  messagingSenderId: "543137308160"
+  storageBucket: "studio-2645965705-c0aaa.firebasestorage.app",
+  messagingSenderId: "543137308160",
+  appId: "1:543137308160:web:da3b5045ee9ae592e102e5"
 };
 
 export const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
